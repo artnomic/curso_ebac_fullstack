65dad78c87ac73090de2c0ae85342dcbfee64a64
arthur.berson <arthurpe44@gmail.com>
2024-06-24 19:28:44 -0300

    feat: use gulp to compile sass

--- 16 - Módulo 16/module/gulpfile.js
+++ 16 - Módulo 16/module/gulpfile.js
@@ -1,21 +1,38 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
 
-function funcaoPadrao (callback) {
-    setTimeout(function() {
+
+function compilaSass() {
+    return gulp.src("./source/styles/main.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "compressed",
+        }))
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest("./build/styles"));
+}
+
+function funcaoPadrao(callback) {
+    setTimeout(function () {
         console.log("Executando via Gulp");
         callback();
     }, 2000)
 }
 
-function dizOi (callback) { 
+function dizOi(callback) {
     console.log("Olá Gulp!");
     dizTchau();
     callback();
 }
 
-function dizTchau () {
+function dizTchau() {
     console.log("Tchau Gulp");
 }
 
 exports.default = gulp.parallel(funcaoPadrao, dizOi);
 exports.oi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch("./source/styles/*.scss", {ignoreInitial: false}, gulp.series(compilaSass));
+}
