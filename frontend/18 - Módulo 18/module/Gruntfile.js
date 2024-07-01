10913b59e1a293e5e9430a0e41422d3d9ed9a08d
artnomic <arthurpe44@gmail.com>
2024-07-01 18:35:35 -0300

    feat: less and sass compressed

--- frontend/18 - Módulo 18/module/Gruntfile.js
+++ frontend/18 - Módulo 18/module/Gruntfile.js
@@ -1,16 +1,48 @@
+const {
+    option
+} = require("grunt");
+
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                option: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        }
     })
 
-    grunt.registerTask("olaGrunt", function() {
-        const done = this.async ();
+    grunt.registerTask("olaGrunt", function () {
+        const done = this.async();
 
-        setTimeout(function() {
+        setTimeout(function () {
             console.log("Olá Grunt");
             done();
         }, 5000);
     })
 
-    grunt.registerTask("default", ["olaGrunt"]);
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-sass");
+
+    grunt.registerTask("default", ["less", "sass"]);
 }
