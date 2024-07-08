323c3af65782d5adc25fea1b2c0f2b9ae2d2e302
artnomic <arthurpe44@gmail.com>
2024-07-08 17:10:09 -0300

    feat: initiate grunt project

--- frontend/18 - Módulo 18/module/Gruntfile.js
+++ frontend/18 - Módulo 18/module/Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         less: {
             development: {
                 files: {
-                    'main.css': 'main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
             production: {
@@ -16,38 +16,14 @@ module.exports = function (grunt) {
                     compress: true,
                 },
                 files: {
-                    'main.min.css': 'main.less'
+                    'dist/styles/main.min.css': 'src/styles/main.less'
                 }
             }
-        },
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                files: {
-                    'main2.css': 'main.scss'
-                }
-            }
-        },
-        concurrent: {
-            target: ["less", "sass", "olaGrunt"]
         }
     })
 
-    grunt.registerTask("olaGrunt", function () {
-        const done = this.async();
-
-        setTimeout(function () {
-            console.log("Olá Grunt");
-            done();
-        }, 5000);
-    })
-
     grunt.loadNpmTasks("grunt-contrib-less");
-    grunt.loadNpmTasks("grunt-contrib-sass");
-    grunt.loadNpmTasks("grunt-concurrent");
 
-
-    grunt.registerTask("default", ["concurrent"]);
+    grunt.registerTask("default", ["less:development"]);
+    grunt.registerTask("build", ["less:production"]);
 }
