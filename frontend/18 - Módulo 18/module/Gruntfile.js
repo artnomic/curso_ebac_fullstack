99dbd48ef6cc7abe7b1883e07e0eb81b87b889ce
artnomic <arthurpe44@gmail.com>
2024-07-23 16:34:06 -0300

    feat: sorteador

--- frontend/18 - Módulo 18/module/Gruntfile.js
+++ frontend/18 - Módulo 18/module/Gruntfile.js
@@ -57,9 +57,14 @@ module.exports = function (grunt) {
             dist: {
                 options: {
                     patterns: [{
-                        match: 'ENDERECO_DO_CSS',
-                        replacement: './styles/main.min.css'
-                    }]
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
                 },
                 files: [{
                     expand: true,
@@ -80,7 +85,14 @@ module.exports = function (grunt) {
                 }
             }
         },
-        clean: ['prebuild']
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
@@ -88,7 +100,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
