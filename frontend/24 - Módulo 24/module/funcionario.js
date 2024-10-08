b01c1bf7a9faf4efa0a0f02de42aec5eaae807bb
artnomic <arthurpe44@gmail.com>
2024-10-08 18:15:23 -0300

    feat: create constructor

--- frontend/24 - Módulo 24/module/funcionario.js
+++ frontend/24 - Módulo 24/module/funcionario.js
@@ -7,7 +7,17 @@ function Pessoa(nome) {
 
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
-    this.salario = salario;
+    // this.salario = salario;
+    let _salario = salario;
+
+    this.getSalario = function () {
+        return `O salário do ${this.nome} é ${_salario}`;
+    }
+    this.setSalario = function (novoSalario) {
+        if (typeof novoSalario === 'number') {
+            _salario = novoSalario;
+        }
+    }
 
     this.dizCargo = function () {
         console.log(this.cargo);
@@ -18,3 +28,4 @@ function Funcionario(nome, cargo, salario) {
 
 const funcionario1 = new Funcionario("Arthur", "Programador", 1000);
 console.log(funcionario1);
+console.log(funcionario1.getSalario());
