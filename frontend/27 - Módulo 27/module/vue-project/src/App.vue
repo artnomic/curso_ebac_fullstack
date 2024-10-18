84a32cf2c28032cf17f27d12305c354f4716d627
artnomic <arthurpe44@gmail.com>
2024-10-18 20:00:26 -0300

    feat: renderização condicional

--- frontend/27 - Módulo 27/module/vue-project/src/App.vue
+++ frontend/27 - Módulo 27/module/vue-project/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
+import { reactive } from "vue";
 
 const nome = "Arthur";
 const meuObj = {
   nome: "Arthur",
   sobrenome: "Berson",
   idade: 23,
-  filmeFavorito: "Creed"
-}
+  filmeFavorito: "Creed",
+};
 
 const nomeCompleto = `${nome} ${meuObj.sobrenome}`;
 
@@ -14,27 +15,62 @@ function dizOi(nome) {
   return `Oi, ${nome}!`;
 }
 
-const depay = "https://soucg.com.br/wp-content/uploads/2024/10/Memphis-Depay-pode-deixar-o-Corinthians-apos-crise-com-patrocinadora-768x576.jpg"
-const garro = "https://stcotvfoco.com.br/2024/10/rodrigo-garro.jpg"
+const depay =
+  "https://soucg.com.br/wp-content/uploads/2024/10/Memphis-Depay-pode-deixar-o-Corinthians-apos-crise-com-patrocinadora-768x576.jpg";
+const garro = "https://stcotvfoco.com.br/2024/10/rodrigo-garro.jpg";
 
 const gostaDoMemphisDepay = false;
 const gostaDoGarro = false;
 
+// let contador = 0;
+const estado = reactive({
+  contador: 0,
+  email: ' ',
+});
+
+function incrementa() {
+  estado.contador++;
+}
+
+function decrementa() {
+  estado.contador--;
+}
+
+function alteraEmail(evento) {
+  estado.email = evento.target.value;
+}
+
 </script>
 
 <template>
-  <h1>Olá {{nomeCompleto}}</h1>
-  <img v-if="gostaDoMemphisDepay" v-bind:src="depay" alt="Memphis Depay">
-  <img v-else-if="gostaDoGarro" v-bind:src="garro" alt="Rodrigo Garro">
+  <h1>Olá {{ nomeCompleto }}</h1>
+  <img v-if="gostaDoMemphisDepay" v-bind:src="depay" alt="Memphis Depay" />
+  <img v-else-if="gostaDoGarro" v-bind:src="garro" alt="Rodrigo Garro" />
   <h2 v-else>Não curte nenhum jogador do Corinthians.</h2>
 
-  <p>Oi, {{nomeCompleto}}!</p>
-  <p>{{nome}} tem {{meuObj.idade}} anos.</p>
-  <p>O filme favorito é {{meuObj.filmeFavorito}}.</p>
-  <p>{{dizOi(nome)}}</p>
+  <br />
+  <hr />
+
+  <p>Oi, {{ nomeCompleto }}!</p>
+  <p>{{ nome }} tem {{ meuObj.idade }} anos.</p>
+  <p>O filme favorito é {{ meuObj.filmeFavorito }}.</p>
+  <p>{{ dizOi(nome) }}</p>
+
+  <br />
+  <hr />
+
+  {{ estado.contador }}
+
+  <button type="button" @click="incrementa()">+</button>
+  <button type="button" @click="decrementa()">-</button>
+
+  <br />
+  <hr />
+
+  {{ estado.email }}
+  <!-- <input type="email" @keyup="evento => estado.email = evento.target.value" /> -->
+  <input type="email" @keyup="alteraEmail" />
 </template>
-
-
 
 <style scoped>
 img {
