4f8634b6c068bc7d447c7e424d9dcf561a222f72
artnomic <arthurpe44@gmail.com>
2024-10-18 19:38:16 -0300

    feat: renderização condicional

--- frontend/27 - Módulo 27/module/vue-project/src/App.vue
+++ frontend/27 - Módulo 27/module/vue-project/src/App.vue
@@ -17,14 +17,27 @@ function dizOi(nome) {
 const depay = "https://soucg.com.br/wp-content/uploads/2024/10/Memphis-Depay-pode-deixar-o-Corinthians-apos-crise-com-patrocinadora-768x576.jpg"
 const garro = "https://stcotvfoco.com.br/2024/10/rodrigo-garro.jpg"
 
+const gostaDoMemphisDepay = false;
+const gostaDoGarro = false;
+
 </script>
 
 <template>
   <h1>Olá {{nomeCompleto}}</h1>
-  <img v-bind:src="depay" alt="Memphis Depay">
-  <img :src="garro" alt="Rodrigo Garro">
+  <img v-if="gostaDoMemphisDepay" v-bind:src="depay" alt="Memphis Depay">
+  <img v-else-if="gostaDoGarro" v-bind:src="garro" alt="Rodrigo Garro">
+  <h2 v-else>Não curte nenhum jogador do Corinthians.</h2>
+
+  <p>Oi, {{nomeCompleto}}!</p>
+  <p>{{nome}} tem {{meuObj.idade}} anos.</p>
+  <p>O filme favorito é {{meuObj.filmeFavorito}}.</p>
+  <p>{{dizOi(nome)}}</p>
 </template>
 
-<style scoped>
 
+
+<style scoped>
+img {
+  max-width: 200px;
+}
 </style>
