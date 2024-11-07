520086416c5350c38bbfd0a87189e3ab6c258c4a
artnomic <arthurpe44@gmail.com>
2024-11-07 17:18:40 -0300

    feat: finish project

---
+++ frontend/28 - Módulo 28/exercise/react-imc/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from './components/Header'
+import IMC from './components/IMC'
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <IMC />
+      <Formulario />
+    </>
+  )
+}
+
+export default App
