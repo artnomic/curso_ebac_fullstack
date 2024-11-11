811f2e3553c928da2bcded8af2f5101034b76900
artnomic <arthurpe44@gmail.com>
2024-11-11 18:35:05 -0300

    init portifolio project

---
+++ frontend/29 - Módulo 29/module/portifolio/src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/artnomic.png" alt="GitHub - Avatar" />
+    <Titulo>Arthur Berson</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ frontend/29 - Módulo 29/module/portifolio/src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, aperiam,
+      laboriosam soluta voluptatem blanditiis dolores odit impedit ipsum
+      exercitationem perferendis accusantium dolorem sint quae quidem deserunt,
+      consequatur numquam distinctio quos!
+    </p>
+  </section>
+)
+
+export default Sobre
