2acda0d48f279cdb2a31f92a5f051bc74a7b7ca7
artnomic <arthurpe44@gmail.com>
2024-11-14 18:36:35 -0300

    create text components

--- frontend/29 - Módulo 29/module/portifolio/src/containers/Sidebar/index.tsx
+++ frontend/29 - Módulo 29/module/portifolio/src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/artnomic.png" alt="GitHub - Avatar" />
-    <Titulo>Arthur Berson</Titulo>
+    <Titulo fontSize={20}>Arthur Berson</Titulo>
   </aside>
 )
 
--- frontend/29 - Módulo 29/module/portifolio/src/containers/Sobre/index.tsx
+++ frontend/29 - Módulo 29/module/portifolio/src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, aperiam,
       laboriosam soluta voluptatem blanditiis dolores odit impedit ipsum
       exercitationem perferendis accusantium dolorem sint quae quidem deserunt,
       consequatur numquam distinctio quos!
-    </p>
+    </Paragrafo>
   </section>
 )
 
