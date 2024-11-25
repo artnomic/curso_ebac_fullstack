c85745a4a4ba3a51440ffc6f02df9428780edde2
artnomic <arthurpe44@gmail.com>
2024-11-25 15:20:35 -0300

    feat: create a theme

--- frontend/29 - Módulo 29/module/portifolio/src/App.tsx
+++ frontend/29 - Módulo 29/module/portifolio/src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import lightTheme from './themes/light'
+import darkTheme from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [usingDarkTheme, setUsingDarkTheme] = useState(false)
+
+  function changeTheme() {
+    setUsingDarkTheme(!usingDarkTheme)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={usingDarkTheme ? darkTheme : lightTheme}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar changeTheme={changeTheme} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ frontend/29 - Módulo 29/module/portifolio/src/themes/dark.ts
@@ -0,0 +1,17 @@
+const darkTheme = {
+  corPrincipal: '#fff',
+  corSecundaria: '#eee',
+  corDeFundo: '#282a35',
+  corDeFundoBotao: '#fff',
+  corDaBorda: '#c1c1c1'
+}
+
+export default darkTheme
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
