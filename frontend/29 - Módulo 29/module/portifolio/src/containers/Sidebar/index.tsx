400890f109cfce4be450c01786d199d4264655fb
artnomic <arthurpe44@gmail.com>
2024-11-25 14:52:59 -0300

    feat: create a sidebar

--- frontend/29 - Módulo 29/module/portifolio/src/containers/Sidebar/index.tsx
+++ frontend/29 - Módulo 29/module/portifolio/src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/artnomic.png" alt="GitHub - Avatar" />
-    <Titulo fontSize={20}>Arthur Berson</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Arthur Berson</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        artnomic
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={16}>
+        Front-end developer, loves to build cool stuffs
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
