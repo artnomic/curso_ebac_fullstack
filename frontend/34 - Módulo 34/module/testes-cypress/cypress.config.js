a7f14227bd21009db905152bde28057f2f2dbe57
artnomic <arthurpe44@gmail.com>
2025-02-11 14:25:23 -0300

    feat: test with cypress to report and runs

--- frontend/34 - Módulo 34/module/testes-cypress/cypress.config.js
+++ frontend/34 - Módulo 34/module/testes-cypress/cypress.config.js
@@ -6,4 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/report',
+    overwrite: true,
+    hmtl: true,
+    json: false,
+    timestamp: 'ddmmyyyy_HHMMss'
+  }
 });
